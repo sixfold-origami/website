8ab6ae1e1e9ee2be4159c42fc1125cbada8e2c9e
sixfold <rosepeck1997@gmail.com>
2024-06-11 12:13:41 -0400

    Add dark theme by default

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import ThemeRegistry from '@/components/themeRegistry/ThemeRegistry';
 import AppBar from '@/components/AppBar';
+import getInitColorSchemeScript from '@mui/system/cssVars/getInitColorSchemeScript';
 
 export const metadata = {
 	title: 'sixfold',
@@ -8,8 +9,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en">
+		<html lang="en" data-joy-color-scheme="dark">
 			<body>
+				{getInitColorSchemeScript({
+					// These properties are normally set when importing from @mui/material,
+					// but we have to set manually because we are importing from @mui/system.
+					attribute: 'data-joy-color-scheme',
+					modeStorageKey: 'joy-mode',
+					colorSchemeStorageKey: 'joy-color-scheme',
+					// All options that you pass to CssVarsProvider you should also pass here.
+					defaultMode: 'dark',
+				})}
 				<ThemeRegistry>
 					<AppBar />
 					{children}
