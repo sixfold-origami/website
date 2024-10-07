df874938016cabc7b9e4e1eddb7a4c97ea3f89ae
sixfold <rosepeck1997@gmail.com>
2024-10-07 13:40:08 -0400

    Re-add posts

---
+++ src/posts/index.js
@@ -0,0 +1,34 @@
+import { readdirSync } from 'fs';
+import path from 'path';
+
+export function getPosts() {
+	const dir = path.resolve('./src', 'posts');
+
+	const posts = readdirSync(dir)
+		.map((file) => {
+			return file.split('.');
+		})
+		// eslint-disable-next-line no-unused-vars
+		.filter(([filename, ext]) => ext === 'mdx')
+		// eslint-disable-next-line no-unused-vars
+		.map(([filename, ext]) => filename);
+
+	return posts;
+}
+
+export function getSortedPostMetadata() {
+	return getPosts()
+		.map((filename) => {
+			const post = require(`@/posts/${filename}.mdx`);
+			return { ...post.metadata, ...{ slug: filename } };
+		})
+		.sort((a, b) => {
+			if (a.date === b.date) {
+				return 0;
+			} else if (a.date < b.date) {
+				return 1;
+			} else if (a.date > b.date) {
+				return -1;
+			}
+		});
+}
