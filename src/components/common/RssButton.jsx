96e2471ce82a644f64df6663b4259d487abc962c
sixfold <rosepeck1997@gmail.com>
2025-02-22 15:58:42 -0800

    Add RSS link to navbar

---
+++ src/components/common/RssButton.jsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { Button, Snackbar } from '@mui/joy';
+import { Rss as RssIcon } from '@phosphor-icons/react/dist/ssr';
+import { ROOT_URL } from '@/consts';
+import * as React from 'react';
+
+export default function RssButton(props) {
+	let [barOpen, setBarOpen] = React.useState();
+
+	return (
+		<Button
+			{...props}
+			onClick={() => {
+				navigator.clipboard.writeText(`${ROOT_URL}/rss`);
+				setBarOpen(true);
+			}}
+		>
+			<RssIcon />
+			<Snackbar
+				open={barOpen}
+				autoHideDuration={3000}
+				onClose={() => setBarOpen(false)}
+				color="primary"
+				sx={{ top: 56 }}
+				anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+			>
+				RSS link copied to clipboard!
+			</Snackbar>
+		</Button>
+	);
+}
