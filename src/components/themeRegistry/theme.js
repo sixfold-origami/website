f5462696530549c776c419360120807ed984b63b
sixfold <rosepeck1997@gmail.com>
2025-02-22 21:32:35 -0800

    Remove dead palette code

--- src/components/themeRegistry/theme.js
+++ src/components/themeRegistry/theme.js
@@ -42,19 +42,6 @@ const theme = extendTheme({
 					800: '#065f46',
 					900: '#064e3b',
 				},
-				// Tailwind zinc
-				// neutral: {
-				// 	50: '#fafafa',
-				// 	100: '#f4f4f5',
-				// 	200: '#e4e4e7',
-				// 	300: '#d4d4d8',
-				// 	400: '#a1a1aa',
-				// 	500: '#71717a',
-				// 	600: '#52525b',
-				// 	700: '#3f3f46',
-				// 	800: '#27272a',
-				// 	900: '#18181b',
-				// },
 				// Tailwind stone
 				neutral: {
 					50: '#fafaf9',
