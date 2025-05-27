c0e4b6d7912e4eb67f121aba85f0350bef11284e
sixfold <rosepeck1997@gmail.com>
2025-05-27 11:35:22 -0700

    Add secrets

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,17 @@
 import { Box, Typography } from '@mui/joy';
+import Link from '@/components/common/Link';
 import * as React from 'react';
 
 export default async function NotFound() {
 	return (
 		<Box sx={{ textAlign: 'center' }}>
 			<Typography level="h1">404</Typography>
-			<Typography>Page not found</Typography>
+			<Typography sx={{ mb: 2 }}>Page not found</Typography>
+			<Typography level="body-sm">
+				(Yes, this technically counts as one of the{' '}
+				<Link href="/about">secret pages</Link>, but it is also just the 404
+				page. So, congrats?)
+			</Typography>
 		</Box>
 	);
 }
