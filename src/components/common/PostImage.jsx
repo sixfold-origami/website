383cf66cc0f110464dede18d9c7264b74062d378
sixfold <rosepeck1997@gmail.com>
2025-08-26 00:11:36 -0700

    Write a bit of post

--- src/components/common/PostImage.jsx
+++ src/components/common/PostImage.jsx
@@ -2,26 +2,22 @@ import * as React from 'react';
 import Image from 'next/image';
 import { Box, Typography } from '@mui/joy';
 
-export default async function TextBlock({
-	src,
-	alt,
-	caption,
-	width,
-	height,
-	...props
-}) {
+import Link from '@/components/common/Link';
+
+export default async function TextBlock({ alt, original, caption, ...props }) {
 	return (
 		<Box maxWidth="sm" sx={{ mx: 'auto', my: 3, textAlign: 'center' }}>
-			<Image
-				src={src}
-				alt={alt}
-				width={width}
-				height={height}
-				style={{ width: '100%', height: 'auto' }}
-				{...props}
-			/>
+			<Image alt={alt} style={{ width: '100%', height: 'auto' }} {...props} />
 			<Typography level="body-md">
 				<i>{caption}</i>
+				{original && (
+					<>
+						&nbsp;&nbsp;&nbsp;
+						<Link href={original} target="_blank">
+							<Typography fontSize="sm">[original]</Typography>
+						</Link>
+					</>
+				)}
 			</Typography>
 		</Box>
 	);
