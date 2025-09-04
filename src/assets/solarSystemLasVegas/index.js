1116e1f6dcd2f3aebc21b280b55f4a20a85737b3
sixfold <rosepeck1997@gmail.com>
2025-09-04 14:38:56 -0700

    More images

--- src/assets/solarSystemLasVegas/index.js
+++ src/assets/solarSystemLasVegas/index.js
@@ -1,5 +1,11 @@
 import theSphere from './the sphere.jpg';
+import mercury from './mercury.jpg';
+import earth from './earth.jpg';
+import pluto from './pluto.jpg';
 
 module.exports = {
 	theSphere,
+	mercury,
+	earth,
+	pluto,
 };
--- src/components/common/PostImage.jsx
+++ src/components/common/PostImage.jsx
@@ -4,9 +4,16 @@ import { Box, Typography } from '@mui/joy';
 
 import Link from '@/components/common/Link';
 
-export default async function TextBlock({ alt, original, caption, ...props }) {
+export default async function TextBlock({
+	alt,
+	original,
+	caption,
+	maxWidth = 'sm',
+	...props
+}) {
 	return (
-		<Box maxWidth="sm" sx={{ mx: 'auto', my: 3, textAlign: 'center' }}>
+		<Box maxWidth={maxWidth} sx={{ mx: 'auto', my: 3, textAlign: 'center' }}>
+			{/* Pass the image alt manually to make the linter happy */}
 			<Image alt={alt} style={{ width: '100%', height: 'auto' }} {...props} />
 			<Typography level="body-md">
 				<i>{caption}</i>
