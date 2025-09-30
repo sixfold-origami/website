f3777c5dca77914520554d35fa234a3ec37a106c
sixfold <rosepeck1997@gmail.com>
2025-09-30 00:15:12 -0700

    Write mercury and some of venus

--- src/assets/solarSystemLasVegas/index.js
+++ src/assets/solarSystemLasVegas/index.js
@@ -1,11 +1,15 @@
 import theSphere from './the sphere.jpg';
 import mercury from './mercury.jpg';
+import starbucks from './starbucks.png';
+import venus from './venus.png';
 import earth from './earth.jpg';
 import pluto from './pluto.jpg';
 
 module.exports = {
 	theSphere,
 	mercury,
+	starbucks,
+	venus,
 	earth,
 	pluto,
 };
